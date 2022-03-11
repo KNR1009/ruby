fa5b72d4b5baddef86a78d82465cba99f318686d
伊東和成 <itokazunari5375@icloud.com>
2022-03-12 08:41:32 +0900

    commit

--- test.rb
+++ test.rb
@@ -1,1 +1,15 @@
-p /./ =~ "\nRuby"
+class Car
+  def accele
+    print("アクセルを踏みました¥n")
+  end
+end
+
+class Soarer < Car
+  def accele
+    super
+    print("加速しました¥n")
+  end
+end
+
+t = Soarer.new
+t.accele
